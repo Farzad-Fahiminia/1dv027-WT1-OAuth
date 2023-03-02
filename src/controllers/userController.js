fb96c7e507168ab8e936eb47c56f17161e004793
Farzad Fahiminia <ff222cb@student.lnu.se>
2023-03-02 11:30:01 +0100

    Add activities page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,12 +7,21 @@
 
 // import { User } from '../models/user.js'
 import fetch from 'node-fetch'
-// import cryptoRandomString from 'crypto-random-string'
+import cryptoRandomString from 'crypto-random-string'
 
 /**
  * Encapsulates a controller.
  */
 export class UsersController {
+  /**
+   * Creates an instance of UsersController.
+   *
+   * @memberof UsersController
+   */
+  constructor () {
+    this.state = cryptoRandomString({ length: 128 })
+  }
+
   /**
    * Handle the redirect.
    *
@@ -21,7 +30,7 @@ export class UsersController {
    * @param {Function} next - Express next middleware function.
    */
   login (req, res, next) {
-    res.redirect(`https://gitlab.lnu.se/oauth/authorize?client_id=${process.env.APP_ID}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code&scope=${process.env.REQUESTED_SCOPE}`)
+    res.redirect(`https://gitlab.lnu.se/oauth/authorize?client_id=${process.env.APP_ID}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code&scope=${process.env.REQUESTED_SCOPE}&state=${this.state}`)
   }
 
   /**
@@ -34,7 +43,12 @@ export class UsersController {
   async callback (req, res, next) {
     console.log('TEST CALLBACK')
     console.log(req.query.code)
-    // const codeVerifier = cryptoRandomString({ length: 128 })
+    console.log('REQ STATE ', req.query.state)
+
+    if (req.query.state !== this.state) {
+      req.session.destroy()
+      res.redirect('../')
+    }
 
     const body = {
       client_id: process.env.APP_ID,
@@ -44,10 +58,6 @@ export class UsersController {
       grant_type: 'authorization_code'
     }
 
-    console.log(body)
-
-    // console.log('code ', codeVerifier)
-
     const response = await fetch('https://gitlab.lnu.se/oauth/token', {
       method: 'POST',
       headers: {
@@ -55,19 +65,16 @@ export class UsersController {
       },
       body: JSON.stringify(body)
     })
-    console.log(response.status)
+
     const accessToken = await response.json()
 
-    console.log(accessToken)
-
-    if (response.status === 200) {
-      req.session.accessToken = accessToken
-      req.session.loggedIn = true
+    if (response.status !== 200) {
+      req.session.destroy()
+      res.redirect('../')
     }
 
-    console.log(req.session)
-
-    // res.render('./users/account')
+    req.session.accessToken = accessToken
+    req.session.loggedIn = true
     res.redirect('./profile')
   }
 
@@ -109,6 +116,51 @@ export class UsersController {
       // res.render('issues/index', { viewData })
 
       res.render('./users/profile', { viewData })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Handle the redirect to activities page.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async activities (req, res, next) {
+    console.log('REQ ', req.session.accessToken)
+    const token = req.session.accessToken?.access_token
+    try {
+      let data = await fetch('https://gitlab.lnu.se/api/v4/events', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token
+        }
+      })
+      data = await data.json()
+
+      // console.log('ACTIVITIES ', data)
+
+      // const viewData = {
+      //   action_name: data.action_name,
+      //   created_at: data.created_at,
+      //   target_title: data.target_title,
+      //   target_type: data.target_type
+      // }
+
+      const viewData = data.map(activity => ({
+        action_name: activity.action_name,
+        created_at: activity.created_at,
+        target_title: activity.target_title,
+        target_type: activity.target_type
+      }))
+
+      console.log('VIEW ', viewData)
+
+      res.render('./users/activities', { viewData })
+      // res.render('./users/activities')
     } catch (error) {
       next(error)
     }
